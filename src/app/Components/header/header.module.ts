9d18494e2f0ede856d886fc7e5a5e0cf182a29c3
“Luiz-om” <luizfernando_martins@hotmail.com>
2024-07-21 07:16:16 -0400

    Inicio Projeto

---
+++ src/app/Components/header/header.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({  //decorator 
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeaderModule { }
